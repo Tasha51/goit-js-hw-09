b9122f892d16464c9cfc1b5f434777a5d53ef872
Nataliia Tatsiienko <natasha51star29@gmail.com>
2022-08-03 21:52:32 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', startChangeColor);
+
+stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+    // this.removeEventListener('click', startChangeColor);
+
+    this.disabled = true;
+};
+
+function stopChangeColor() {
+    clearInterval(timerId);
+    // startBtn.addEventListener('click', startChangeColor);
+
+    startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
